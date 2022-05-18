2904ebc37d20af1a8012c2a87ac868648772636d
saiprasad-psp <77107261+saiprasad-psp@users.noreply.github.com>
2022-05-18 12:05:30 +0200

    Update charlieStreamlit.py

--- charlieStreamlit.py
+++ charlieStreamlit.py
@@ -9,8 +9,16 @@ import streamlit as st
 import plotly.express as px
 import requests,json
 
+option="charlie"
+strategyCapitalDic={"charlie":150000}
+
+botCapital=strategyCapitalDic[option]
+
+st.title("**LIVE PERFORMANCE OF "+option.upper()+"**")
+st.write("**Capital used is "+str(botCapital)+"**")
+
 #pnl_url = 'https://pythonbucketbh.s3.ap-south-1.amazonaws.com/allPnl.json'
-pnl_url = st.secrets["read_url"]
+pnl_url = "https://objectstorage.eu-frankfurt-1.oraclecloud.com/p/ZZ_qqCxYDxwQKLpojdGmjDJ74Ok9BoXPeNfZE_i4Qa4YRwRKNBWj7KGP9xicinCs/n/frj64xgqdjz2/b/bucket-20220326-2113/o/pnl.json"
 pnl_data=requests.get(pnl_url)
 pnl_df_t=pd.DataFrame.from_dict(json.loads(pnl_data.text))
 pnl_df=pnl_df_t.T
@@ -21,10 +29,8 @@ pnl_df=pnl_df_t.T
 #option = st.selectbox(
     # 'Select Strategy',
     # ('ALL', 'BNFStraddle', 'MeanReversion', 'IntradayTrend'))
-option="charlie"
-strategyCapitalDic={"charlie":150000}
 
-botCapital=strategyCapitalDic[option]
+charges = st.checkbox('With Charges', value=False, help="Assumption charges are 51 Rs per Day.")
 strat_pnl_Df=pnl_df[[option]]
 strat_pnl_Df.dropna(inplace=True)
 strat_df=strat_pnl_Df
@@ -34,6 +40,9 @@ strat_df['pdTime']=pd.to_datetime(strat_df.index,format="%Y-%m-%d")
 strat_df.sort_values('pdTime',inplace=True)
 #strat_df[option]=(botCapital/100)*strat_df[option].astype(float)
 strat_df["Time"]=strat_df.index
+if charges:
+    strat_df[option]=strat_df[option] - 51.0
+
 strat_df['PNL']=strat_df[option]
 strat_df['cum_pnl']=strat_df[option].cumsum()
 strat_df['PNL %'] = strat_df['PNL'] * 100 / strategyCapitalDic[option]
@@ -85,8 +94,6 @@ month_groups['PNL %'] = month_groups['PNL'] * 100 / strategyCapitalDic[option]
 ##last 30 days pnl
 strat_df=strat_df.reindex(strat_df.index[::-1])
 
-st.title("**LIVE PERFORMANCE OF "+option.upper()+"**")
-st.write("**Capital used is "+str(botCapital)+"**")
 if results_row[-1] < 0:
     color = "#FF3339"
 else:
